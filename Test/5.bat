6db8d9929feb72dd87fa80381bd3b49498bea351
montateoo <matteo.montanelli@mail.polimi.it>
2024-08-31 13:40:11 +0200

    file

---
+++ Test/5.bat
@@ -0,0 +1,19 @@
+@echo off
+cd /d "C:\Users\monta\CLionProjects\Pasticceria\cmake-build-debug"
+
+set startTime=%TIME%
+Pasticceria.exe < open5.txt > TestOut.txt
+set endTime=%TIME%
+
+for /F "tokens=1-4 delims=:.," %%a in ("%startTime%") do (
+    set /A "startInSeconds=(((%%a*60)+1%%b %% 100)*60+1%%c %% 100)"
+)
+
+for /F "tokens=1-4 delims=:.," %%a in ("%endTime%") do (
+    set /A "endInSeconds=(((%%a*60)+1%%b %% 100)*60+1%%c %% 100)"
+)
+
+set /A duration=endInSeconds-startInSeconds
+echo Il programma ha impiegato %duration% secondi.
+
+pause
